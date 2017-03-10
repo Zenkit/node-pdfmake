08387c803eeef1a43b32598e539c30d1ee9bcbfe
Philipp Beck <dertieran@gmail.com>
2017-03-10 19:01:34 +0100

    :tada: This is where it all begins ...

---
+++ README.md
@@ -0,0 +1,54 @@
+# Node PDFMake
+This is a wrapper around [pdfmake](https://github.com/bpampuch/pdfmake) to allow
+~~client/~~**server** side PDF ~~printing~~ **creation** in pure JavaScript.
+
+## Usage
+
+For the `docDefinition` options see the [pdfmake documentation](https://github.com/bpampuch/pdfmake#document-definition-object).
+
+### createPdf(docDefinition)
+
+```js
+var pdfDocGenerator = nodePDFMake.createPdf(docDefinition);
+```
+
+### getBuffer([options], [callback(err, buffer)])
+
+Returns the created PDF as a `Buffer`.
+
+```js
+pdfDocGenerator.getBuffer(function (err, buffer) {
+  // ...
+});
+```
+
+If `callback` is not defined, then it returns a promise.
+
+```js
+pdfDocGenerator.getBuffer()
+  .then(function (buffer) {
+    // ...
+  })
+```
+
+### getBase64([options], [callback(err, data)])
+
+Returns the created PDF as a `base64` encoded string.
+
+```js
+pdfDocGenerator.getBase64(function (err, data) {
+  // ...
+});
+```
+
+If `callback` is not defined, then it returns a promise.
+
+```js
+pdfDocGenerator.getBase64()
+  .then(function (data) {
+    // ...
+  })
+```
+
+## License
+[MIT](https://github.com/dertieran/node-pdfmake/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict'
+
+var path = require('path')
+var PdfPrinter = require('pdfmake')
+var streamToArray = require('stream-to-array')
+
+var defaultFonts = {
+  Roboto: {
+    normal: path.join(__dirname, '/fonts/Roboto-Regular.ttf'),
+    bold: path.join(__dirname, '/fonts/Roboto-Medium.ttf'),
+    italics: path.join(__dirname, '/fonts/Roboto-Italic.ttf'),
+    bolditalics: path.join(__dirname, '/fonts/Roboto-MediumItalic.ttf')
+  }
+}
+
+function Document (docDefinition) {
+  this.docDefinition = docDefinition
+}
+
+Document.prototype._createDoc = function (options) {
+  options = options || {}
+  var printer = new PdfPrinter(defaultFonts)
+  var doc = printer.createPdfKitDocument(this.docDefinition)
+  var parts = streamToArray(doc)
+  doc.end()
+  return parts
+}
+
+Document.prototype.getBuffer = function (options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+
+  var deferred = this._createDoc(options)
+    .then(function (parts) {
+      return Buffer.concat(parts)
+    })
+
+  if (typeof callback === 'function') {
+    deferred.then(function (result) {
+      callback(null, result)
+    }, callback)
+  }
+
+  return deferred
+}
+
+Document.prototype.getBase64 = function (options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+
+  var deferred = this.getBuffer(options)
+  .then(function (buffer) {
+    return buffer.toString('base64')
+  })
+
+  if (typeof callback === 'function') {
+    deferred.then(function (result) {
+      callback(null, result)
+    }, callback)
+  }
+
+  return deferred
+}
+
+module.exports = {
+  createPdf: function (docDefinition) {
+    return new Document(docDefinition)
+  }
+}
